0389d25da07adc2e05fd3f56f542a9be14e6d4e1
TomasGiordano <tomas.giordano@boreal-it.com>
2021-11-27 02:38:30 -0300

    fix

--- src/app/pretty-json.pipe.ts
+++ src/app/pretty-json.pipe.ts
@@ -5,9 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PrettyJsonPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return JSON.stringify(value, null, 2)
-    .replace(/ /g, '&nbsp;') // note the usage of `/ /g` instead of `' '` in order to replace all occurences
-    .replace(/\n/g, '<br/>'); // same here
+  /**
+   * @param value A value of any type to convert into a JSON-format string.
+   */
+   transform(value: any): string {
+    return JSON.stringify(value, null, 2);
   }
 }
