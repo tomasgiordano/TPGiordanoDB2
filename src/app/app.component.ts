56ecf9e9001ff199c5430b91ae307d7c666c8615
TomasGiordano <tomas.giordano@boreal-it.com>
2021-11-27 01:30:15 -0300

    arreglos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,53 @@ export class AppComponent {
         this.jsonResponse = JSON.stringify(data)
       })
   }
+
+  getMotivoCambioPlan(){
+    return this.http.get(this.urlServer + "/motivoCambioPlan")
+    .subscribe((data: any) => {
+      this.jsonResponse = JSON.stringify(data)
+    })
+  }
+
+  getLugarTicketCambioPlan(){
+    return this.http.get(this.urlServer + "/lugarTicketCambioPlan")
+    .subscribe((data: any) => {
+      this.jsonResponse = JSON.stringify(data)
+    })
+  }
+
+  getBeneficiosClientes(){
+    return this.http.get(this.urlServer + "/beneficiosClientes")
+    .subscribe((data: any) => {
+      this.jsonResponse = JSON.stringify(data)
+    })
+  }
+
+  getOperacionesFallidas(){
+    return this.http.get(this.urlServer + "/operacionesFallidas")
+    .subscribe((data: any) => {
+      this.jsonResponse = JSON.stringify(data)
+    })
+  }
+
+  getClientesEnEEUU(){
+    return this.http.get(this.urlServer + "/clientesEnEEUU")
+    .subscribe((data: any) => {
+      this.jsonResponse = JSON.stringify(data)
+    })
+  }
+
+  getCapitalFlorida(){
+    return this.http.get(this.urlServer + "/capitalFlorida")
+    .subscribe((data: any) => {
+      this.jsonResponse = JSON.stringify(data)
+    })
+  }
+
+  getTicketsFibraOptica(){
+    return this.http.get(this.urlServer + "/ticketsFibraOptica")
+    .subscribe((data: any) => {
+      this.jsonResponse = JSON.stringify(data)
+    })
+  }
 }
